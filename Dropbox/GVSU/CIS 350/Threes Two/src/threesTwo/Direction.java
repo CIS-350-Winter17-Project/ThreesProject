8023b5f0043de94dbbb7eefbbfd028dc086da975
Scott Lumsden <Scott.w.lumsden@gmail.com>
2017-03-02 18:03:47 -0500

    Update Direction.java

--- Dropbox/GVSU/CIS 350/Threes Two/src/threesTwo/Direction.java
+++ Dropbox/GVSU/CIS 350/Threes Two/src/threesTwo/Direction.java
@@ -1,5 +1,17 @@
 package threesTwo;
 
+/**********************************************************************
+* This is an enumerated class type that dictates what direction was 
+* entered. 
+* 
+* @author Anthony Blanton
+* @author Scott Lumsden
+* @author Dylan Shoup
+* 
+*@version 0.1 March 3, 2017
+*********************************************************************/
+
 public enum Direction {
 	UP, DOWN, LEFT, RIGHT;
 }
+
