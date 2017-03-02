2c5bc7341596bd94070c36e49a31a9626ef8f321
Scott Lumsden <Scott.w.lumsden@gmail.com>
2017-03-02 18:04:39 -0500

    Updated class with Javadoc comments.

--- Dropbox/GVSU/CIS 350/Threes Two/src/threesTwo/GameStatus.java
+++ Dropbox/GVSU/CIS 350/Threes Two/src/threesTwo/GameStatus.java
@@ -1,5 +1,17 @@
 package threesTwo;
 
+/**********************************************************************
+* This is an enumerated class type that tells the status of the game. 
+* When the game is in GAME_OVER, the game is finished and it resets
+* itself. 
+* 
+* @author Anthony Blanton
+* @author Scott Lumsden
+* @author Dylan Shoup
+* 
+*@version 0.1 March 3, 2017
+*********************************************************************/
+
 public enum GameStatus {
 	IN_PROGRESS, GAME_OVER;
 }
