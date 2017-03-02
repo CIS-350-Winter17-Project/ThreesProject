f3318bb3084fd2f6f372adefadafdc102c85ac37
Scott Lumsden <Scott.w.lumsden@gmail.com>
2017-03-02 18:05:00 -0500

    Update ThreesTwo.java

--- Dropbox/GVSU/CIS 350/Threes Two/src/threesTwo/ThreesTwo.java
+++ Dropbox/GVSU/CIS 350/Threes Two/src/threesTwo/ThreesTwo.java
@@ -2,17 +2,23 @@ package threesTwo;
 
 import javax.swing.SwingUtilities;
 
-public class ThreesTwo {
+/**********************************************************************
+ * Main "Threes" class. Used to initiate the board, and ultimately 
+ * run the game. 
+ * 
+ *
+ * @author Scott Lumsden
+ * @author Dylan Shoup
+ * @author Anthony Blanton
+ * @version 0.1 March 3, 2017
+ *********************************************************************/
 
-	/**********************************************************************
-	 * Main "Threes" class. Used only for testing purposes.
-	 * 
-	 *
-	 * @author Scott Lumsden
-	 * @author Dylan Shoup
-	 * @author Anthony Blanton
-	 * @version 0.1
-	 *********************************************************************/
+public class ThreesTwo {
+	
+	/******************************************************************
+	 * Begins the Threes game when run. 
+	 * @param args NA
+	 *****************************************************************/
 
 	public static void main(String[] args) {
 		// This is going to be my main class for our group project.
